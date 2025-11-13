e34a90cd19ce33d60b8ec96b62c6fd9ff6f063ac
Dalibor Povolny <dalibor.povolny@gmail.com>
2025-11-13 23:05:26 +0100

    add link to music page and initial music page structure

--- music.php
+++ music.php
@@ -1,21 +1,21 @@
 <?php
 $name = "hello";
+$name = ""
 ?>
 
 <html>
     <head>
-        <title>Index Page, This is HOME!!!!!!</title>
+        <title>The Wreck of the Edmund Fitzgerald</title>
     </head>
 
     <body>
-        <h1>____HOMEE___!!!</h1>
-
+        <h1>The Wreck of the Edmund Fitzgerald</h1>
+        <p>On the November 10th 1975 the Edmunt Fitzgerald sank</p>
 
         <a href="/hello.php"> Hello </a><br>
         <a href="/other.php"> Other </a><br>
         <a href="/notes.php"> Notes </a><br>
         <a href="/form.php"> Form </a><br>
-        <a href="/music.php"> Music </a><br>
 
     </body>
 </html>
